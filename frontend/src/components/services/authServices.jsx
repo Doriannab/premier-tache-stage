f4304d8a09e248b721bcce0599cd9c33a92c3866
Anna Dev <148019294+Doriannab@users.noreply.github.com>
2024-01-26 12:34:04 +0000

    message22

--- frontend/src/components/services/authServices.jsx
+++ frontend/src/components/services/authServices.jsx
@@ -1,15 +1,15 @@
 import axios from 'axios';
 
 
-const SERVER_URL = 'https://run-web.onrender.com/api';
+const SERVER_URL = 'https://run-web.onrender.com/';
 
 
 const registerUser = (data)=>{
-    return axios.post(SERVER_URL+'/register',data);
+    return axios.post(SERVER_URL+'api/register',data);
 }
 
 const loginUser = (data)=>{
-    return axios.post(SERVER_URL+'/login',data);
+    return axios.post(SERVER_URL+'api/login',data);
 }
 
 
